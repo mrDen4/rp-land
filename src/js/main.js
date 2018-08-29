5984a7c788cb6074c9cd65952c9d516cfe74fc75
Denis Davydov <den.saint.k@gmail.com>
2018-08-29 15:42:51 +0300

    add js for menu

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+//Кнопка заказать звонок
+$('.header__burger').on('click', function() {
+    $('.menu').toggleClass('menu--active');
+});
+
+$('.item__link').on('click', function() {
+    $('.menu').toggleClass('menu--active');
+});
+
+$('.menu__exit').on('click', function() {
+    $('.menu').toggleClass('menu--active');
+});
+
+// //Кнопка узнать цену
+// $('.buy__btn').on('click', function() {
+//     $('.modal__card-price').toggleClass('modal__card-price--active');
+//     $('.bg__modal').toggleClass('bg__modal--active');
+// });
+
+// //Выход из popup
+// $('.bg__modal').on('click', function() {
+//     $('.modal__card').removeClass('modal__card--active');
+//     $('.modal__card-price').removeClass('modal__card-price--active');
+//     $('.bg__modal').removeClass('bg__modal--active');
+// });
+
+// $('.modal__exit').on('click', function() {
+//     $('.modal__card').removeClass('modal__card--active');
+//     $('.modal__card-price').removeClass('modal__card-price--active');
+//     $('.bg__modal').removeClass('bg__modal--active');
+// });
